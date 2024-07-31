c6f042bb36460a4b3816bbbfff906ca93a330c11
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-07-31 17:24:24 +0200

    Messo in pagina + Css

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ va applicato uno sconto del 40% per gli over 65.*/
 //Check collegamento JS
 console.log ('JS OK')
 
-//Preparazione
+// Preparazione :Recupero l'elemento di interesse nel DOM
+const resultElement = document.getElementById ('result');
+console.log('resultElement');
+
 let totalPrice 
 
 //Raccolta dati: chiedo il numero di km da percorrere e l'tà del passeggero
@@ -32,5 +35,9 @@ console.log ('ticketPricescontati')
 totalPrice = ticketPrice.toFixed(2);
 console.log ('totalprice')
 
-//Output
+//Output: appare in pagina
+console.log(`Il prezzo del biglietto è: € ${totalPrice}`);
+
+resultElement.innerHTML = `Il prezzo del biglietto è: <strong> ${totalPrice}</strong>` ;
+
 
